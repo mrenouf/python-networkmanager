408a4c796c0674c6352722355a37973be72589c4
Mark Renouf <mark.renouf@gmail.com>
2010-02-02 17:47:27 -0500

    Experimenting with NetworkSettings, NetworkConnection, etc

--- src/NetworkManager.py
+++ src/NetworkManager.py
@@ -6,8 +6,8 @@ import dbus
 
 class NetworkManager(object):
 
-	def __init__(self):
-		self.bus = dbus.SystemBus()
+	def __init__(self, bus):
+		self.bus = bus
 		self.proxy = self.bus.get_object(
 			"org.freedesktop.NetworkManager",
 			"/org/freedesktop/NetworkManager")
@@ -48,13 +48,44 @@ class NetworkManagerDevice(object):
 	def disconnect(self):
 		self.device.Disconnect()
 
+class NetworkManagerSettings():
+	def __init__(self, bus):
+		self.proxy = bus.get_object(
+			"org.freedesktop.NetworkManager",
+			"/org/freedesktop/NetworkManagerSettings")
+		
+	def list_connections(self):
+		return self.proxy.ListConnections(dbus_interface='org.freedesktop.NetworkManagerSettings')
+	
+	def add_connection(self, properties):
+		pass
+	
+class NetworkManagerConnection():
+	def __init__(self, bus, connection):
+		self.proxy = bus.get_object("org.freedesktop.NetworkManager", connection);
+	
+	def get_settings(self):
+		"""
+		Returns this connections settings - a{sa{sv}} (String_String_Variant_Map_Map)
+		The nested settings maps describing this object.
+		"""
+		return self.proxy.GetSettings(dbus_interface='org.freedesktop.NetworkManagerSettings.Connection')
+		
+	def delete(self):
+		self.proxy.Delete(dbus_interface='org.freedesktop.NetworkManagerSettings.Connection')
+
+	def update(self, properties):
+		self.proxy.Update(properties, dbus_interface='org.freedesktop.NetworkManagerSettings.Connection')
+
+
 class NetworkManagerDeviceWired(NetworkManagerDevice):
 	def __init__(self, bus, object_path):
 		super(NetworkManagerDeviceWired, self).__init__(bus, object_path)
 
 
 if __name__ == "__main__":
-	nm = NetworkManager()
+	bus = dbus.SystemBus()
+	nm = NetworkManager(bus)
 	print nm.WirelessEnabled
 	print nm.WirelessHardwareEnabled
 	print nm.ActiveConnections
@@ -63,6 +94,20 @@ if __name__ == "__main__":
 	for device in nm.get_devices():
 		print device.Udi
 		print device.device_type()
-
+		
+	set = NetworkManagerSettings(bus)
+	print set.list_connections()[0]
 	
+	print "=========="
+	con = NetworkManagerConnection(bus, set.list_connections()[0])
+
+	set = con.get_settings()
+	print set['connection']['type']
+	print set['connection']['uuid']
+	print set['connection']['id']
+	
+	print set['802-3-ethernet']['mac-address']
+	#for k in con.get_settings().keys():
+	#	for v in k.keys():
+	#		print v
 
